805c0a547a582fbee094104183c93db4028204ca
Arthur Alexandre Capelli Dos Santos <arthur.santos@cwi.com.br>
2023-04-11 11:03:07 -0300

    Criando repositorio

---
+++ src/test/java/br/com/valida/cpf/CpfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valida.cpf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
