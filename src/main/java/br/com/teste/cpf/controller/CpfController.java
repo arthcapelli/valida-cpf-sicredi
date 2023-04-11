d666116ac0c6359dc627e32ac115d63655d9ef29
Arthur Alexandre Capelli Dos Santos <arthur.santos@cwi.com.br>
2023-04-11 14:57:35 -0300

    Simplificando codigo e adicionando tratamento de response na api sicredi

--- src/main/java/br/com/teste/cpf/controller/CpfController.java
+++ src/main/java/br/com/teste/cpf/controller/CpfController.java
@@ -1,17 +1,13 @@
 package br.com.teste.cpf.controller;
 
-import br.com.teste.cpf.representation.response.CpfResponse;
 import br.com.teste.cpf.service.CpfService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping()
@@ -22,9 +18,7 @@ public class CpfController {
 
     @GetMapping("{cpf}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<CpfResponse> consultaCpf(@PathVariable String cpf) {
-        return Optional.ofNullable(cpfService.consultaCpf(cpf))
-                .map(cpfResponse -> ResponseEntity.ok().body(cpfResponse))
-                .orElseGet(() -> ResponseEntity.notFound().build());
+    public boolean consultaCpf(@PathVariable String cpf) {
+        return cpfService.consultaCpf(cpf);
     }
 }
--- src/main/java/br/com/teste/cpf/service/CpfService.java
+++ src/main/java/br/com/teste/cpf/service/CpfService.java
@@ -1,29 +1,17 @@
 package br.com.teste.cpf.service;
 
-import br.com.teste.cpf.domain.Status;
-import br.com.teste.cpf.mapper.CpfMapper;
-import br.com.teste.cpf.representation.response.CpfResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Random;
 import java.util.regex.Pattern;
 
 @Service
 public class CpfService {
 
-    @Autowired
-    private CpfMapper cpfMapper;
-
     private static final Pattern CPF_PATTERN = Pattern.compile("\\d{3}\\.?\\d{3}\\.?\\d{3}-?\\d{2}");
 
-    public CpfResponse consultaCpf(String cpf) {
+    public boolean consultaCpf(String cpf) {
 
-        if (!verificaCpfValido(cpf)) {
-            return null;
-        }
-
-        return randomizaRetorno();
+        return verificaCpfValido(cpf);
     }
 
     public static boolean verificaCpfValido(String cpf) {
@@ -61,17 +49,6 @@ public class CpfService {
         }
 
         return cpf.charAt(9) - '0' == firstVerifier && cpf.charAt(10) - '0' == secondVerifier;
-    }
-
-    public CpfResponse randomizaRetorno() {
-        Random random = new Random();
-        int resultado = random.nextInt(2);
-
-        if (resultado == 0) {
-            return cpfMapper.toResponse(Status.ABLE_TO_VOTE.toString());
-        }
-        return cpfMapper.toResponse(Status.UNABLE_TO_VOTE.toString());
-
     }
 
 }
